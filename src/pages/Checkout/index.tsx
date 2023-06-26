f13565e7492cdcc4532a4cf32a908ed852ec13d4
Adriângelo Pereira da Silva <126906223+Adriangelo1982@users.noreply.github.com>
2023-06-26 19:43:38 -0300

    Initial commit

---
+++ src/pages/Checkout/index.tsx
@@ -0,0 +1,13 @@
+import { Header } from "../../components/Header/Index";
+
+
+export function CheckoutPage() {
+    return (
+      <div>
+        <Header />
+        <h1>CheckoutPage</h1>
+      </div>
+      
+    )
+  }
+  //não usado
